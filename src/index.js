89382ad3f59d522ed17e8f669f6508531ee68b5a
Htet Bo Bo Aung <boboa3473@gmail.com>
2023-12-06 14:47:04 +0630

    Create The Movie-App

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 import _ from 'lodash';
 import './style.css';
-import Icon from './icon.png';
 
+//  function component() {
+//    const element = document.createElement('div');
+
+//   // Lodash, now imported by this script
+//    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+//    return element;
+//  }
+
+//  document.body.appendChild(component());
