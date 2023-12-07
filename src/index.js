0366acf3b1bfe36b096b4a4d20a992edce86ae62
Htet Bo Bo Aung <boboa3473@gmail.com>
2023-12-07 21:18:56 +0630

    Create the reservation button

--- src/index.js
+++ src/index.js
@@ -1,42 +1,50 @@
-// import _ from 'lodash';
-// import './style.css';
-
 const container = document.querySelector(".container");
 
 async function getData (){
      try {
-          const items = await axios.get('https://api.tvmaze.com/shows');
-          console.log(items.data);
-          items.data.map((item) => {
+          const response = await axios.get('https://api.tvmaze.com/shows');
+          return response.data;
+     } catch (error) {
+          console.error(error);
+     }
+}
+
+async function displayFunction() {
+     try {
+          const data = await getData();
+          data.forEach((item) => {
                const cardList = document.createElement("article");
                cardList.className = "card-container";
                cardList.innerHTML = `
                <div class="img-div">
                <img src=${item.image.medium} alt="" class="image">
           </div>
-
+     
           <div class="info-div">
                <h2 class="info-title">${item.name}</h2>
-
+     
           <ul class="model-list">
             ${item.genres.map((genre) => `<p class="model-item">${genre}</p>`).join('')}
           </ul>
-
+     
           <ul class="comment-section">
                <button class="comment-box">Comments</button>
-
+     
                <li class="like-list">
                <i class="fa-solid fa-heart"></i>
                     <p id="like-number">0</p>
                </li>
+
+               <button class="reservation-btn">Reservation</button>
           </ul>
           </div>`
-          console.log(item);
           container.append(cardList);
-          })
-     } catch (error) {
-          console.error(error);
+          });
      }
+     catch(e){
+          console.error(e);
+     }
+     
 }
 
-getData();
+window.addEventListener("DOMContentLoaded", displayFunction);
