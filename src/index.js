f76ff54e5801a9df28dea9b300fe75a937ca07e3
Htet Bo Bo Aung <boboa3473@gmail.com>
2023-12-11 21:00:50 +0630

    Create likenumber

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
 const container = document.querySelector(".container");
 const commentContainer = document.querySelector(".comment-wrapper");
 const comPopUp = document.querySelector(".comment-popup");
+const getAppId = "UT0vpKlsx8hbbbeuzzTd";
 
-async function getData(){
-     try {
-          const response = await axios.get('https://api.tvmaze.com/shows');
-          localStorage.setItem("response", JSON.stringify(response));
-          return response.data;
-     } catch (error) {
-          console.error(error);
-     }
+async function getData() {
+  try {
+    const response = await axios.get('https://api.tvmaze.com/shows');
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 async function displayFunction() {
   try {
     const data = await getData();
     data.forEach((item, index) => {
-      let { name, image, genres} = item;
+      let { name, image, genres } = item;
       const cardList = document.createElement("article");
       cardList.className = "card-container";
       cardList.setAttribute("data-index", index);
@@ -36,7 +36,7 @@ async function displayFunction() {
             <button class="comment-box" data-index="${index}">Comments</button>
 
             <li class="like-list">
-              <i class="fa-solid fa-heart"></i>
+              <i class="fa-solid fa-heart" data-set=${item.id}></i>
               <p class="like-number">0</p>
             </li>
 
@@ -45,8 +45,7 @@ async function displayFunction() {
         </div>`;
       container.append(cardList);
 
-
-//comment section
+      // comment section
       const displayBtn = cardList.querySelector(".comment-box");
       displayBtn.addEventListener("click", (e) => {
         let index = e.target.dataset.index;
@@ -72,6 +71,8 @@ async function displayFunction() {
               <button class="tailer-btn">Watch Trailer</button>
             </ul>
 
+            <div class="userComment-div"></div>
+
             <form class="comment-form">
               <h2 class="comment-form-title">Comments</h2>
 
@@ -80,15 +81,13 @@ async function displayFunction() {
               <button class="submit-btn">Submit</button>
             </form>
 
-            <div class="userComment-div"></div>
           </div>
 
           <button class="close-btn">X</button>
         `;
         commentContainer.append(popUpcard);
 
-
-//btn section
+        // btn section
         const closeBtn = popUpcard.querySelector(".close-btn");
         closeBtn.addEventListener("click", () => {
           commentContainer.classList.remove("display");
@@ -98,15 +97,22 @@ async function displayFunction() {
 
       const btn = cardList.querySelector(".fa-heart");
       const numberDisplay = cardList.querySelector(".like-number");
-      let cardLikes = parseInt(localStorage.getItem(`cardLikes_${index}`)) || 0;
 
-      numberDisplay.textContent = cardLikes;
-
-      btn.addEventListener("click", () => {
-        cardLikes++;
-        numberDisplay.textContent = cardLikes;
-        localStorage.setItem(`cardLikes_${index}`, cardLikes);
+      btn.addEventListener("click", async (e) => {
+        let itemId = e.target.dataset.set;
+        await postRequest(itemId);
+        const likeCount = await getRequest(itemId);
+        numberDisplay.textContent = likeCount;
       });
+
+//showComment 
+
+      const usernames = document.querySelector(".inputname");
+      const userComments = document.querySelector(".comment");
+      const submitBtn = document.querySelector(".submit-btn");
+      const userCommentDiv = document.querySelector(".userComment-div");
+      const commentsList = document.createElement("div");
+      
     });
   } catch (e) {
     console.error(e);
@@ -115,31 +121,40 @@ async function displayFunction() {
 
 window.addEventListener("DOMContentLoaded", displayFunction);
 
-async function postRequest() {
-  const data = [
-    {
-        "likes": 5,
-        "item_id": "item1"
-    },
-]
+async function postRequest(id) {
   try {
-    const postresult = await axios.post("https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/",data);
-    console.log(postresult.data);
+    const postresult = await axios.post(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${getAppId}/likes/`, {
+      item_id: id,
+    });
+    console.log(postresult);
   } catch (e) {
     console.error(e);
   }
 }
 
-const getAppId = "UT0vpKlsx8hbbbeuzzTd"; 
-async function getRequest() {
+async function getRequest(id) {
   try {
     const response = await axios.get(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${getAppId}/likes/`);
-    console.log(response);
+    const likeData = response.data.filter((like) => like.item_id === id);
+    console.log(likeData.length);
+    return likeData.length;
   } catch (error) {
-    console.error('Error fetching data:', error);
+    console.error(error);
   }
 }
 
-getRequest();
+async function CommentPostRequest(id, name, text) {
+  try {
+    const response = await axios.post(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${getAppId}/comments/`, {
+      item_id: id,
+      username: name,
+      comment: text,
+    });
+    console.log(response);
+  } catch (e) {
+    console.error(e);
+  }
+}
 
-postRequest();
+
+displayFunction();
